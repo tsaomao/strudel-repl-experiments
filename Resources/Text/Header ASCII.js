52c200b39a78dd08447b08142a6ee70a156b6e6e
Malcolm Gin <malcolm.gin@gmail.com>
2025-08-28 14:59:51 -0700

    Added Ascii header for future credit on work.

---
+++ Resources/Text/Header ASCII.js
@@ -0,0 +1,26 @@
+//                                         X   X   
+// XXXXXXX XXXXXX    XXXXXXXXXXX    X   X  X   X   
+//   X X    X             X          X X XXXXXXXXX 
+//   X X    X            X            X    X   X   
+// XXXXXXX  X      XXXXXXXXXXXXXXX   X X   X   X   
+// X X X X  XXXX        X   X       X  X           
+// X X X X  X  X       X     X     X   X  XXXXXXX  
+// XXXXXXX  X  X     XX   X   XX       X  X  X  X  
+//    X    X X X   XX XXXXXXXXX XX    XX  X  X  X  
+//    X    X  XX      X   X   X      X X  X  X  X  
+// XXXXXXX X   X      X   X   X     X  X  XXXXXXX  
+//    X    X   X      XXXXXXXXX    X   X  X  X  X  
+//    X    X X X X    X   X            X  X  X  X  
+//    XXXX XX  X X        X    X       X  X  X  X  
+// XXXX    X   X X  XXXXXXXXXXXXX   X X   XXXXXXX  
+//  X           XX   X          X    X    X     X  
+//
+//     _____                                      
+//    /__   \___  __ _  ___     /\/\   __ _  ___  
+//      / /\/ __|/ _` |/ _ \   /    \ / _` |/ _ \ 
+//     / /  \__ \ (_| | (_) | / /\/\ \ (_| | (_) |
+//     \/   |___/\__,_|\___/  \/    \/\__,_|\___/ 
+//                                                
+// Created by: Tsao Mao
+// Date: 8/28/2025
+// License: Free reuse and derivation. Would prefer you credit me, if possible, by name at least if not ASCII banners.
