fe31f4d5610015c59b1a78cf9e1815be7fac7a5d
Malcolm Gin <malcolm.gin@gmail.com>
2025-09-02 17:19:37 -0700

    Updated Amen Experiment.

--- Experiments/20250902-AmenBreak.js
+++ Experiments/20250902-AmenBreak.js
@@ -19,28 +19,19 @@
 // per: https://www.elephantdrums.co.uk/blog/guides-and-resources/amen-break-drum-groove/
 // See also: https://ukf.com/read/10-things-you-need-to-know-about-the-amen-break/
 //
-// DRAFT - can't get drumbeat notation to work properly
+// DRAFT - First approximation - long form, every hit mapped out in 4 measures
 setCps (150/60/4)
 
-const CPATTERN = "----------------------------------------------------------x-----"
-const RPATTERN = "x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x---x-x-"
-const SPATTERN = "----x--x-x--x--x----x--x-x--x--x----x--x-x----x--x--x--x-x----x-"
-const BPATTERN = "x-x-------xx----x-x-------xx----x-x-------x-------xx------x-----"
-
-const CLAP_GAIN            = slider(2.354, 0.1, 5)
-const RIM_GAIN             = slider(2.4226, 0.1, 5)
-const SNARE_GAIN           = slider(2.8538, 0.1, 5)
-const BASS_DRUM_GAIN       = slider(3.1919, 0.1, 5)
-
-const CLAP       = s("tr808_cr").struct(CPATTERN).gain(CLAP_GAIN)
-const RIM        = s("tr808_rd").struct(RPATTERN).gain(RIM_GAIN)
-const SNARE      = s("tr808_sd").struct(SPATTERN).gain(SNARE_GAIN)
-const BASS_DRUM  = s("tr808_bd").struct(BPATTERN).gain(BASS_DRUM_GAIN)
-
-stack(
-      BASS_DRUM, 
-      CLAP, 
-      RIM, 
-      SNARE,
-     )
-
+$CLAP: s("<- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - cr - - - - ->*16")
+  .bank("akaixr10")
+  .gain(5)  
+$RIM: s("<hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - - - hh - hh ->*16")
+  .bank("ace")
+  .gain(1)
+$SNARE: s("<- - - - sd - - sd - sd - - sd - - sd - - - - sd - - sd - sd - - sd - - sd - - - - sd - - sd - sd - - - - sd - - sd - - sd - - sd - sd - - - - sd ->*16")
+  .bank("9000")
+  .gain(2.5)
+$BASS_DRUM: s("<bd - bd - - - - - - - bd bd - - - - bd - bd - - - - - - - bd bd - - - - bd - bd - - - - - - - bd - - - - - - - bd bd - - - - - - bd - - - - ->*16")
+  .bank("9000")
+  .gain(5)
+  .sustain(2)
