5446ba557be477e9cc8b16f4c6747b2a5fc3d643
Malcolm Gin <malcolm.gin@gmail.com>
2025-09-04 09:02:18 -0700

    Update Amen Break with mini notatiion.

--- Experiments/20250902-AmenBreak.js
+++ Experiments/20250902-AmenBreak.js
@@ -1,11 +1,11 @@
 //     _____                                      
 //    /__   \___  __ _  ___     /\/\   __ _  ___  
-//      / /\/ __|/ _` |/ _ \   /    \ / _` |/ _ \ 
+//      / /\/ __|/ _` |/ _ \   /    \ / _` |/ _ \
 //     / /  \__ \ (_| | (_) | / /\/\ \ (_| | (_) |
-//     \/   |___/\__,_|\___/  \/    \/\__,_|\___/ 
+//     \/   |___/\__,_|\___/  \/    \/\__,_|\___/
 //                                                
 // Created by: Tsao Mao
-// Date: 9/2/2025
+// Date: 9/4/2025
 // License: Free reuse and derivation. Would prefer you credit me, if possible, by name at least if not ASCII banners.
 //     No reuse willingly granted to fascists.
 // Wordpress: https://geekblog.malcolmgin.com/strudel-repl/
@@ -18,20 +18,23 @@
 // B |o-o-------oo----|o-o-------oo----|o-o-------o-----|--oo------o-----|
 // per: https://www.elephantdrums.co.uk/blog/guides-and-resources/amen-break-drum-groove/
 // See also: https://ukf.com/read/10-things-you-need-to-know-about-the-amen-break/
-//
-// DRAFT - First approximation - long form, every hit mapped out in 4 measures
+//           https://tidalcycles.org/docs/reference/mini_notation/
+// DRAFT - Second approximation - Mini Notation
 setCps (150/60/4)
 
-$CLAP: s("<- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - cr - - - - ->*16")
+$:  s("{~!48 ~!10 cr ~!5}/4")
   .bank("akaixr10")
-  .gain(5)  
-$RIM: s("<hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - hh - - - hh - hh ->*16")
+  .gain(2)
+  ._punchcard({ labels:1})
+$: s("{{hh ~}!24 {hh ~}!5 ~!2 {hh ~}!2}/4")
   .bank("ace")
-  .gain(1)
-$SNARE: s("<- - - - sd - - sd - sd - - sd - - sd - - - - sd - - sd - sd - - sd - - sd - - - - sd - - sd - sd - - - - sd - - sd - - sd - - sd - sd - - - - sd ->*16")
+  .gain(0.15)
+  ._punchcard({ labels:1})
+$: s("{{~!4 sd ~ ~ sd ~ sd ~ ~ sd ~ ~ sd}!2 {~!4 sd ~ ~ sd ~ sd ~ ~ ~ ~ sd ~}!2}/4")
   .bank("9000")
   .gain(2.5)
-$BASS_DRUM: s("<bd - bd - - - - - - - bd bd - - - - bd - bd - - - - - - - bd bd - - - - bd - bd - - - - - - - bd - - - - - - - bd bd - - - - - - bd - - - - ->*16")
+  ._punchcard({ labels:1})
+$: s("{{bd ~}!2 ~!6 bd!2 ~!4 {bd ~}!2 ~!6 bd!2 ~!4 {bd ~}!2 ~!6 bd ~ ~!6 bd!2 ~!6 bd ~!5}/4")
   .bank("9000")
   .gain(5)
-  .sustain(2)
+  ._punchcard({ labels:1})
